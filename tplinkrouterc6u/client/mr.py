a2fbc41d72de8dfd7b759b8857fef8ad7da38707
PAFriedrichMueller <mueller@partikel-art.de>
2025-09-13 11:27:51 +0200

    printing 2

--- tplinkrouterc6u/client/mr.py
+++ tplinkrouterc6u/client/mr.py
@@ -542,7 +542,7 @@ class TPLinkMRClientBase(AbstractRouter):
         retry = 0
         while retry < self.REQUEST_RETRIES:
             # send the request
-            self._logger.error("retrycount:" + retry)
+            self._logger.error("retrycount:" + str(retry))
             if method == 'POST':
                 r = self.req.post(url, data=data, headers=headers, timeout=self.timeout, verify=self._verify_ssl)
             elif method == 'GET':
@@ -555,7 +555,7 @@ class TPLinkMRClientBase(AbstractRouter):
                     and '<title>500 Internal Server Error</title>' not in r.text
                     and '<title>406 Not Acceptable</title>' not in r.text):
                 break
-            self._logger.error("fail:"+retry+ " status_code:"+ r.status_code)
+            self._logger.error("fail:"+str(retry)+ " status_code:"+ str(r.status_code))
             sleep(0.1)
             retry += 1
 
